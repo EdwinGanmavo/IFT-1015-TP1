3c52075eacdcf32f1c3057209bd5535aa9366811
EdwinGanmavo <70912343+EdwinGanmavo@users.noreply.github.com>
2021-10-17 13:12:26 -0400

    Update labyrinthe.py

--- labyrinthe.py
+++ labyrinthe.py
@@ -1,3 +1,4 @@
+import turtle
 def iota(n):
     if n >= 0:
         return range(n)
@@ -22,4 +23,36 @@ def retirer(tab, x):
     else:
         print("error, x must be in tab, nothing have been deleted")
         return tab
-
+def carree(largeurCase):
+    for i in range(4):
+         turtle.lt(90)
+         turtle.fd(largeurCase)
+def positionnement(largeurCase):
+    for b in range(1):
+        turtle.pu()
+        turtle.fd(largeurCase)
+        turtle.pd()
+def positionnement2(largeurCase,nx):
+    turtle.pu()
+    turtle.lt(180)
+    turtle.fd(nx*largeurCase)
+    turtle.rt(270)
+    turtle.fd(largeurCase)
+    turtle.lt(90)
+    turtle.fd(largeurCase)
+    turtle.pd()
+def grille(largeurCase,nx,ny):
+    for a in range(ny-1):
+         for y in range(nx-1):
+            carree(largeurCase)
+            positionnement(largeurCase)
+         for b in range(1):
+            carree(largeurCase)
+         positionnement2(largeurCase,nx)
+    for s in range(1):
+        for y in range(nx - 1):
+            carree(largeurCase)
+            positionnement(largeurCase)
+        for b in range(1):
+            carree(largeurCase)
+grille(20,5,5)
